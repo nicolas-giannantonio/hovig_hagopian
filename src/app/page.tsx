e0c19efe4faf916c50ea44957e1e793cc5d730c5
nicolas-giannantonio <nicogiaicloud@gmail.com>
2025-02-16 21:19:39 +0100

    videos on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Projects from "@/components/Projects/Projects";
 import { PROJECTS_QUERY } from "@/lib/queries";
 import { client } from "@/lib/sanity/client";
+import { extractVimeoIdAndToken, getVideoLink } from "@/lib/vimeo";
 
 export const dynamic = "force-dynamic";
 
@@ -10,6 +11,27 @@ export default async function Home() {
   });
 
   const projects = selected[0].projects;
+
+  await Promise.all(
+    projects.map(
+      async (project: {
+        project: {
+          hover_video: string;
+        };
+      }) => {
+        const vimeoInfo = extractVimeoIdAndToken(project.project.hover_video);
+        const link = await getVideoLink(vimeoInfo.videoId, vimeoInfo.token);
+
+        const video720p =
+          link?.play?.progressive.find(
+            (video: { rendition: string; link: string }) =>
+              video.rendition === "720p",
+          ) || link?.play?.progressive[0];
+
+        project.project.hover_video = video720p?.link;
+      },
+    ),
+  );
 
   return (
     <div>
