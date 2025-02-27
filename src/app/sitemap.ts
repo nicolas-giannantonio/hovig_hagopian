233246b1212513f179d34800f3a09295c4f118ce
nicolas-giannantonio <nicogiaicloud@gmail.com>
2025-02-27 14:08:27 +0100

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,43 +3,43 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www/hovighagopian.com",
+      url: "https://www.hovighagopian.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://www/hovighagopian.com/resume",
+      url: "https://www.hovighagopian.com/resume",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://www/hovighagopian.com/contact",
+      url: "https://www.hovighagopian.com/contact",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://www/hovighagopian.com/music-video",
+      url: "https://www.hovighagopian.com/music-video",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
     {
-      url: "https://www/hovighagopian.com/fiction",
+      url: "https://www.hovighagopian.com/fiction",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
     {
-      url: "https://www/hovighagopian.com/pub",
+      url: "https://www.hovighagopian.com/pub",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
     {
-      url: "https://www/hovighagopian.com/film",
+      url: "https://www.hovighagopian.com/film",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.6,
